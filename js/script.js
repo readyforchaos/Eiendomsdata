3aa7c514c0ecc71b1b5e1aa62abbf68562b06451
Anders Gill <gill@outlook.com>
2020-04-22 07:49:11 +0200

    Add basic parser in vanilla js to DOM
    * Parse JSON object from API to DOM

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
 // Initial Get-response from GeoNorge Adresse API to test status 200 = OK
 document.getElementById("btnGetAddress").onclick = function getAddress() {
     var requestOptions = {
@@ -5,8 +13,20 @@ document.getElementById("btnGetAddress").onclick = function getAddress() {
         redirect: 'follow'
       };
       
+      const ul = document.getElementById('addressList')
       fetch("https://ws.geonorge.no/adresser/v1/sok?adressenavn=harbitzalléen&nummer=18", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+        .then(response => response.json())
+        .then(function(data) {
+            let adresser = data.adresser;
+            return adresser.map(function(adresse) {
+                let li = createNode('li'),
+                span = createNode('span');
+
+            span.innerHTML = adresse.adressetekst; // return adressetekst
+            append(li, span);
+            append(ul, li);
+            })
+        })
+        .catch(error => console.log(JSON.stringify(error)));
+    
 }
